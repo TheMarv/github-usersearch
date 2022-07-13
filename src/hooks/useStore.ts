9a239ba26a25f6e036027138ada300e896c32deb
Marvin Steckel <marvin.steckel@gmail.com>
2022-07-13 14:14:10 +0200

    Implement searching on input

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -2,18 +2,56 @@ import create from 'zustand';
 
 interface SearchState {
   searchTerm: string;
+  searchTermTimeout: NodeJS.Timeout | null;
+  searchResults: any[],
+  loading: boolean,
+  lastRequest: number;
   setSearchTerm: (newSearchTerm: string) => void;
+  fetchSearch: () => void;
 }
 
-export const useStore = create<SearchState>((set) => {
+export const useStore = create<SearchState>((set, get) => {
   return {
     searchTerm: '',
+    searchTermTimeout: null,
+    searchResults: [],
+    loading: false,
+    lastRequest: 0,
     setSearchTerm: (newSearchTerm) => {
       set(state => {
+        if (state.searchTermTimeout) clearTimeout(state.searchTermTimeout);
+        if (newSearchTerm.length > 2) {
+          return {
+            searchTerm: newSearchTerm,
+            searchTermTimeout: setTimeout(get().fetchSearch, 500),
+          }
+        }
         return {
           searchTerm: newSearchTerm,
         };
       });
     },
+    fetchSearch: () => {
+      set(state => {
+        return {
+          loading: true
+        }
+      });
+      const lastRequest = get().lastRequest + 6000 - new Date().getTime();
+      setTimeout(() => {
+        fetch(`https://api.github.com/search/issues?q=${get().searchTerm}&order=asc`)
+          .then(response => response.json())
+          .then(data => {
+            const items: any[] = data.items;
+            set(state => {
+              return {
+                searchResults: [...state.searchResults, items],
+                loading: false,
+                lastRequest: new Date().getTime()
+              }
+            });
+          });
+      }, lastRequest > 0 ? lastRequest : 0);
+    }
   }
 });
