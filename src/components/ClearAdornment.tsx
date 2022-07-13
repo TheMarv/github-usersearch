f31c38b8d440d1bc525cf58be8c11fc57c631888
Marvin Steckel <marvin.steckel@gmail.com>
2022-07-13 14:33:14 +0200

    Add functionality to hide clear if textfield isn't focused

--- src/components/ClearAdornment.tsx
+++ src/components/ClearAdornment.tsx
@@ -3,15 +3,17 @@ import InputAdornment from "@mui/material/InputAdornment";
 
 interface ClearAdornmentProps {
   setSearchTerm: (newSearchTerm: string) => void;
+  hidden: boolean
 }
 
-export default function ClearAdornment({ setSearchTerm }: ClearAdornmentProps) {
+export default function ClearAdornment({ setSearchTerm, hidden }: ClearAdornmentProps) {
   return (
     <InputAdornment
       position="start"
       onClick={() => setSearchTerm('')}
       sx={{
           cursor: 'pointer',
+          display: hidden ? 'none' : 'flex'
         }}>
       <Clear />
     </InputAdornment>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,11 @@
 import TextField from '@mui/material/TextField';
 import { useStore } from '../hooks/useStore';
 import ClearAdornment from './ClearAdornment';
+import { useState } from 'react';
 
 export default function Searchbar() {
+
+  const [ hideClear, setHideClear] = useState(true);
 
   const searchTerm = useStore(state => state.searchTerm);
   const setSearchTerm = useStore(state => state.setSearchTerm);
@@ -18,8 +21,10 @@ export default function Searchbar() {
       onChange={onSearchTermChange}
       fullWidth
       autoComplete='off'
+      onFocus={() => setHideClear(false)}
+      onBlur={() => setHideClear(true)}
       InputProps={{
-        endAdornment: (<ClearAdornment setSearchTerm={setSearchTerm}/>)
+        endAdornment: (<ClearAdornment setSearchTerm={setSearchTerm} hidden={hideClear}/>)
       }}
     />
   )
