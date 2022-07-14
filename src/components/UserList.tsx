1d2fd006aa438306e1291fe0aa0699c2e52e1712
Marvin Steckel <marvin.steckel@gmail.com>
2022-07-14 02:42:33 +0200

    Remove unnecessary code

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -5,21 +5,8 @@ import UserItem from "./UserItem";
 export default function UserList() {
   const searchResults = useStore(state => state.searchResults);
 
-  function handleScroll(event: React.UIEvent<HTMLDivElement, UIEvent>) {
-    const target = event.target as HTMLDivElement;
-    console.log(target.scrollTop, target.scrollHeight - target.clientHeight);
-  }
-
   return (
-    <Grid
-      container
-      spacing={2}
-      onScroll={handleScroll}
-      sx={{
-        overflowY: 'scroll',
-        maxHeight: '100%'
-      }}
-    >
+    <Grid container spacing={2}>
       {searchResults.map(result => (
         <Grid item key={result.id} width="100%">
           <UserItem {...result} />
